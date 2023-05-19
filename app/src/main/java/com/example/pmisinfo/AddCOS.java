7e203b3b12924b3e3d31086864ed9c9af98cd1a3
Mala-Kusum <62510264+Mala-Kusum@users.noreply.github.com>
2023-05-19 18:01:20 +0530

    may16

---
+++ app/src/main/java/com/example/pmisinfo/AddCOS.java
@@ -0,0 +1,14 @@
+package com.example.pmisinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCOS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cos);
+    }
+}
